fbdfa1db6f1a258714e8c9c9a6300f8bab01ba1b
wuuconix <wuuconix@qq.com>
2023-03-22 09:57:05 +0800

    add xray.sh

--- README.md
+++ README.md
@@ -11,3 +11,13 @@ my shell scripts.
 ```
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/wuuconix/shell/main/zsh.sh)"
 ```
+
+**xray.sh**:
+
+> Install xray with customized script automatically
+>
+> xray will follow your system architecture e.g. amd64
+
+```
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/wuuconix/shell/main/xray.sh)"
+```
---
+++ xray.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+# This is a script install xray with customized script automatically
+arch=`dpkg --print-architecture`
+apt install -y unzip && \
+mkdir -p ~/xray && \
+cd ~/xray && \
+curl -OL https://github.com/chaitin/xray/releases/download/1.9.4/xray_linux_$arch.zip && \
+unzip xray_linux_$arch.zip && \
+rm xray_linux_$arch.zip && \
+chmod +x ./xray_linux_$arch && \
+mv xray_linux_$arch xray && \
+curl "https://gist.githubusercontent.com/wuuconix/30fbdc72c96e5fe1e32547baf7f1d7ef/raw/48efd2c0aa71c1092c7d5c8f3a3d133f2030eaee" -o start.sh && \
+chmod +x start.sh && \
+mkdir result && \
+./xray && \
+echo -e "\033[32mCongratulations! Install and Configure Done. \033[0m"
+echo -e "\033[32mcd ~/xray \033[0m"
+echo -e "\033[32m./start.sh https://github.com/wuuconix/shell \033[0m"
